97fd85d0eef11a232acaaee18dde3d078cb9f11a
LukaszKot <lukaszkot03@gmail.com>
2023-07-06 20:35:44 +0200

    Added empty kanban page.

--- manage-me/src/app/app.module.ts
+++ manage-me/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TasksPageComponent } from './tasks-page/tasks-page.component';
 import { AddTasksPageComponent } from './add-tasks-page/add-tasks-page.component';
 import { DisplayTaskPageComponent } from './display-task-page/display-task-page.component';
 import { EditTaskPageComponent } from './edit-task-page/edit-task-page.component';
+import { KanbanPageComponent } from './kanban-page/kanban-page.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { EditTaskPageComponent } from './edit-task-page/edit-task-page.component
     TasksPageComponent,
     AddTasksPageComponent,
     DisplayTaskPageComponent,
-    EditTaskPageComponent
+    EditTaskPageComponent,
+    KanbanPageComponent
   ],
   imports: [
     BrowserModule,
