2d0549bbb8c7f301072092cac5a57eb875328f78
LukaszKot <lukaszkot03@gmail.com>
2023-07-06 20:33:04 +0200

    Fixed state and priority.

--- manage-me/src/app/api.service.ts
+++ manage-me/src/app/api.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Feature } from './models/Feature';
 import { Task } from './models/Task';
 import { Observable } from 'rxjs';
@@ -10,7 +10,10 @@ export class ApiService {
   private features: Feature[] = [];
   private counter = 0;
 
-  constructor() { }
+  constructor() {
+    let data = localStorage.getItem('features');
+    this.features = data ? JSON.parse(data) : [];
+  }
 
   getFeatures(): Observable<Feature[]> {
     return new Observable((observer) => {
@@ -20,8 +23,11 @@ export class ApiService {
 
   addFeature(feature: Feature): Observable<Feature> {
     return new Observable((observer) => {
+      feature.state = +feature.state;
+      feature.priority = +feature.priority;
       this.features.push(feature)
       feature.id = ++this.counter;
+      localStorage.setItem('features', JSON.stringify(this.features))
       observer.next(feature)
     });
   }
@@ -36,15 +42,19 @@ export class ApiService {
   deleteFeature(id: number): Observable<any> {
     return new Observable((observer) => {
       this.features = this.features.filter(x => !(x.id == id));
+      localStorage.setItem('features', JSON.stringify(this.features))
       observer.next()
     });
   }
 
   updateFeature(feature: Feature): Observable<any> {
     return new Observable((observer) => {
+      feature.state = +feature.state;
+      feature.priority = +feature.priority;
       this.features = this.features.filter(x => !(x.id == feature.id));
       this.features.push(feature);
       this.features = this.features.sort((a, b) => a.id! - b.id!)
+      localStorage.setItem('features', JSON.stringify(this.features))
       observer.next()
     });
   }
@@ -59,8 +69,11 @@ export class ApiService {
     return new Observable((observer) => {
       let feature = this.features.find(x => x.id == +task.featureId)
       task.featureId = +task.featureId;
+      task.state = +task.state;
+      task.priority = +task.priority;
       feature?.tasks.push(task)
       task.id = ++this.counter;
+      localStorage.setItem('features', JSON.stringify(this.features))
       observer.next(task)
     });
   }
@@ -75,6 +88,7 @@ export class ApiService {
     return new Observable((observer) => {
       let feature = this.features.find(x => x.tasks.findIndex(x => x.id == id));
       feature!.tasks = feature!.tasks.filter(x => !(x.id == id))
+      localStorage.setItem('features', JSON.stringify(this.features))
       observer.next()
     });
   }
@@ -82,11 +96,14 @@ export class ApiService {
   updateTask(task: Task): Observable<any> {
     return new Observable((observer) => {
       task.featureId = +task.featureId;
+      task.priority = +task.priority;
+      task.state = +task.state;
       let feature = this.features.find(x => x.tasks.filter(x => x.id == task.id).length != 0);
       feature!.tasks = feature!.tasks.filter(x => !(x.id == task.id))
       let newFeature = this.features.find(x => x.id == +task.featureId)
       newFeature?.tasks.push(task)
       newFeature!.tasks = newFeature!.tasks.sort((a, b) => a.id! - b.id!)
+      localStorage.setItem('features', JSON.stringify(this.features))
       observer.next()
     });
   }
